57ac85c24cca786fedb5274c69473c68072af3ea
Syd Logan <slogan621@gmail.com>
2024-08-24 21:09:58 -0700

    add some errors, convert to anyhow and thiserror
    Signed-off-by: Syd Logan <slogan621@gmail.com>

--- src/model/error.rs
+++ src/model/error.rs
@@ -1,5 +1,24 @@
+use polars::error::PolarsError;
+
+/* 
 #[derive(Debug)]
 pub enum ModelError {
     UnableToLoadData,
     InvalidTestTrainSplitArgs,
+    UnableToConvertDataFrameToVec(PolarsError),
+}
+*/
+
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum ModelError {
+    #[error("unable to load data")]
+    UnableToLoadData,
+    #[error("invalid test_train_split argumenmts")]
+    InvalidTestTrainSplitArgs,
+    #[error("unable to convert data frame to vec: {0}")]
+    UnableToConvertDataFrameToVec(PolarsError),
+    #[error("unable to extract field from data at index: {0}")]
+    UnableToExtractFieldFromDataFrame(usize)
 }
